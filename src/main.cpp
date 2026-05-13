78f9738276661fdf41ed812d3b3eac74269eda0d
Mike Day <mikeday370146@gmail.com>
2026-05-13 15:53:51 -0400

    when NDEBUG defined, don't wrap app.Run() in try/catch so debugger shows exact location/callstack of uncaught exceptions

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,25 @@
 ﻿#include "pch.h"
 
+#include <cstdlib>
 #include <iostream>
 
 #include "app.h"
 
 int main()
 {
+#ifdef NDEBUG
     try {
+#endif
+
         Mirael::App app;
         app.run();
+
+#ifdef NDEBUG
     } catch (const std::exception &e) {
         std::cerr << e.what() << std::endl;
         return EXIT_FAILURE;
     }
+#endif
 
     return EXIT_SUCCESS;
 }
