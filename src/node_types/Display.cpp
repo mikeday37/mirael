c66ffa03729c85609ba1b93ac8212dbf2ce2a560
Mike Day <mikeday370146@gmail.com>
2026-05-19 15:23:12 -0400

    impl preliminary Display node

--- src/node_types/Display.cpp
+++ src/node_types/Display.cpp
@@ -14,11 +14,25 @@ void Display::onInit() { inPinId_ = addPin("in", {.direction = PinDirection::Inp
 void Display::onShow()
 {
     ne::BeginNode(getId());
-    ImGui::Text("Display");
     ne::BeginPin(inPinId_, ne::PinKind::Input);
-    ImGui::Text("-> in");
+    ImGui::TextUnformatted("->");
     ne::EndPin();
+    ImGui::SameLine();
+    acceptLatestValue();
+    ImGui::Text("Display: %s", value_.c_str());
     ne::EndNode();
+}
+
+void Display::Core::onFrame(const RunContext &context) {
+    auto it = context.inputs.find(inPinId_);
+    if (it == context.inputs.end())
+        return;
+
+    for (const auto *valueBuffer : it->second) {
+        // TODO: this is allocating per frame - switch to something like an expandable ring buffer or triple buffer
+        channel_->pendingValue.postNew(std::make_unique<std::string>(valueBuffer->getValue()));
+        break; // if there's more than one input, we don't care
+    }
 }
 
 } // namespace Mirael::NodeTypes
--- src/node_types/Display.h
+++ src/node_types/Display.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "Mailbox.h"
 #include "Node.h"
 
 namespace Mirael::NodeTypes
@@ -10,11 +11,38 @@ class Display : public Node
 public:
     static const char *typeName() { return "display"; }
 
+protected:
     void onInit() override;
     void onShow() override;
 
+    struct Channel {
+        Mailbox<std::string> pendingValue{};
+    };
+
+    class Core : public NodeCore
+    {
+    public:
+        Core(PinId inPinId, std::shared_ptr<Channel> channel) : channel_(std::move(channel)), inPinId_(inPinId) {}
+
+        void onFrame(const RunContext &context) override;
+
+    private:
+        PinId inPinId_;
+        std::shared_ptr<Channel> channel_;
+    };
+
+    std::unique_ptr<NodeCore> createCore() { return std::make_unique<Core>(inPinId_, channel_); }
+
 private:
     PinId inPinId_{};
+    std::shared_ptr<Channel> channel_ = std::make_shared<Channel>();
+    std::string value_{};
+
+    void acceptLatestValue()
+    {
+        if (auto taken = channel_->pendingValue.tryAcceptLatest())
+            value_ = std::move(*taken);
+    }
 };
 
 } // namespace Mirael::NodeTypes
