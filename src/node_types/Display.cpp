298feb42646df66adcd8e2ec20c25f182d94ff51
Mike Day <mikeday370146@gmail.com>
2026-06-09 13:08:47 -0400

    impl Core side of first pass image support for Display node

--- src/node_types/Display.cpp
+++ src/node_types/Display.cpp
@@ -18,19 +18,114 @@ void Display::onShow()
     ImGui::TextUnformatted("->");
     ne::EndPin();
     ImGui::SameLine();
-    acceptLatestValue();
-    ImGui::Text("Display: %s", value_.c_str());
+
+    switch (getKind()) {
+        
+        case DataKind::String:
+            fetchLatestStringValue();
+            ImGui::Text("Display: %s", stringValue_.c_str());
+            break;
+
+        case DataKind::Image:
+            ImGui::TextUnformatted("Display:");
+            // TODO: continue here - accept dimensions, move old buffer to graveyard, create new buffer, fetch latest, display via ImGui
+            //ImGui::Image()
+            break;
+    }
+
     ne::EndNode();
+}
+
+Display::Core::ValueInfo Display::Core::getValueInfo(const ValueBuffer *vbuf)
+{
+    ValueInfo info = {.kind = DataKind::None};
+
+    if (!vbuf)
+        return info;
+
+    info.kind = DataKind::String; // we display everything but image tables as a string
+
+    if (!vbuf->isTable())
+        return info;
+
+    auto entryTop = lua_gettop(L);
+
+    vbuf->pushValueToLuaStack(); // [value]
+
+    lua_getfield(L, -1, "_tag"); // [value, _tag]
+    auto tag = lua_tostring(L, -1);
+    if (!tag || std::strcmp(tag, "image")) { // TODO: can the strcmp be done faster since lua interns strings?
+        lua_pop(L, 2);
+        assert(lua_gettop(L) == entryTop);
+        return info;
+    }
+
+    lua_getfield(L, -2, "w"); // [value, _tag, w]
+    int w = static_cast<int>(lua_tointeger(L, -1));
+    lua_getfield(L, -3, "h"); // [value, _tag, w, h]
+    int h = static_cast<int>(lua_tointeger(L, -1));
+    lua_getfield(L, -4, "buf"); // [value, _tag, w, h, buf]
+    const void *pbuf = lua_topointer(L, -1); // TODO: need a safer approach - a user could put non-cdata/non-GC values here
+    lua_pop(L, 5);
+    assert(lua_gettop(L) == entryTop);
+
+    if (pbuf && w > 0 && h > 0) {
+        info.dim.width = static_cast<Dimensions::dim_t>(w);
+        info.dim.height = static_cast<Dimensions::dim_t>(h);
+        info.kind = DataKind::Image;
+        info.pixelData = pbuf;
+    }
+
+    return info;
 }
 
 void Display::Core::onFrame(const RunContext &context)
 {
-    if (auto buf = context.getFirstInput(inPinId_))
-    {
-        auto &sbuf = channel_->stringBuffer;
-        sbuf.getWriteSlot() = buf->toString(); // TODO: change this to write into an existing string to avoid allocations per frame
+    L = context.L;
+
+    auto vbuf = context.getFirstInput(inPinId_); // may return nullptr
+    auto info = getValueInfo(vbuf); // this is safe, as getValueInfo accepts nullptr
+
+    switch (info.kind) {
+        using enum Display::DataKind;
+
+    case None:
+        // intentional nop/fallthrough to end
+        break;
+
+    case String: {
+        auto &sbuf          = channel_->stringBuffer;
+        sbuf.getWriteSlot() = vbuf->toString(); // TODO: change this to write into the existing string to avoid allocations per frame
         sbuf.commitWrite();
+        channel_->dataKind.store(DataKind::String, std::memory_order_release);
+        return;
     }
+
+    case Image: {
+        acceptLatestImageBuffer();
+        bool ready = currentImageBuffer_ && currentImageBuffer_->dim == info.dim;
+        if (ready) {
+            auto &slot = currentImageBuffer_->images.getWriteSlot();
+            std::memcpy(slot.mapped, info.pixelData, static_cast<size_t>(4 * info.dim.width * info.dim.height));
+            // the above assumes no row padding, which the UI is currently expected to ensure
+            currentImageBuffer_->images.commitWrite();
+        } else {
+            releaseImageBuffer();
+            channel_->pendingDimensions.postNew(std::make_unique<Dimensions>(info.dim));
+            // TODO: make a slimmer method of requesting new dimensions - this will spam the channel until it gets what it wants
+            // (acceptable for now because dimension changes are intended to be rare)
+        }
+        channel_->dataKind.store(DataKind::Image, std::memory_order_release);
+        return;
+    }
+
+    default:
+        assert(false); // should have handled all possibilities above
+        break;
+    }
+
+    // set kind = none if we didn't set otherwise and return above
+    channel_->dataKind.store(DataKind::None, std::memory_order_release);
 }
 
 } // namespace Mirael::NodeTypes
--- src/node_types/Display.h
+++ src/node_types/Display.h
@@ -16,20 +16,79 @@ protected:
     void onInit() override;
     void onShow() override;
 
+    enum class DataKind {
+        None,
+        String,
+        Image
+    };
+
+    struct Dimensions {
+        using dim_t = uint32_t;
+        dim_t width, height;
+        bool operator==(const Dimensions &) const = default;
+    };
+
+    struct Image {
+        VkImage image = VK_NULL_HANDLE;
+        VmaAllocation allocation = VK_NULL_HANDLE;
+        void *mapped = nullptr;
+        vk::raii::ImageView view = nullptr;
+        vk::raii::Sampler sampler = nullptr;
+        VkDescriptorSet descriptor = VK_NULL_HANDLE;
+    };
+
+    struct ImageBuffer {
+        Dimensions dim; // immutable upon creation by UI
+        std::atomic<bool> live = true; // core -> node, set to false when the core no longer wants it (wrong dimensions)
+        std::atomic<bool> writing = false; // core -> node, true only while core needs to write to image slot
+        TripleBuffer<Image> images{}; // all images in the buffer have the same dimensions
+    };
+
     struct Channel {
-        TripleBuffer<std::string> stringBuffer{};
+        std::atomic<DataKind> dataKind = DataKind::None; // core -> node
+
+        // string channels
+        TripleBuffer<std::string> stringBuffer{}; // shared
+
+        // image channels
+        Mailbox<Dimensions> pendingDimensions{}; // core -> node - signals need for new ImageBuffer
+        Mailbox<std::shared_ptr<ImageBuffer>> pendingImageBuffer{}; // node -> core - passes back a shared_ptr to the latest ImageBuffer
     };
 
     class Core : public NodeCore
     {
     public:
         Core(PinId inPinId, std::shared_ptr<Channel> channel) : channel_(std::move(channel)), inPinId_(inPinId) {}
+        ~Core() {releaseImageBuffer();}
 
+        struct ValueInfo {
+            DataKind kind;
+            Dimensions dim;
+            const void *pixelData;
+        };
+
+        ValueInfo getValueInfo(const ValueBuffer *vbuf);
         void onFrame(const RunContext &context) override;
 
     private:
         PinId inPinId_;
         std::shared_ptr<Channel> channel_;
+        lua_State *L   = nullptr;
+        std::shared_ptr<ImageBuffer> currentImageBuffer_ = nullptr;
+
+        void releaseImageBuffer() {
+            if (currentImageBuffer_)
+                currentImageBuffer_->live = false;
+            currentImageBuffer_ = nullptr;
+        }
+
+        void acceptLatestImageBuffer() {
+            if (auto latest = channel_->pendingImageBuffer.tryAcceptLatest())
+            {
+                releaseImageBuffer();
+                currentImageBuffer_ = std::move(*latest);
+            }
+        }
     };
 
     std::unique_ptr<NodeCore> createCore() { return std::make_unique<Core>(inPinId_, channel_); }
@@ -37,13 +96,19 @@ protected:
 private:
     PinId inPinId_{};
     std::shared_ptr<Channel> channel_ = std::make_shared<Channel>();
-    std::string value_{};
+    std::string stringValue_{};
 
-    void acceptLatestValue()
+    void fetchLatestStringValue()
     {
         auto result = channel_->stringBuffer.fetchLatestReadSlot();
         if (result.isNew)
-            value_ = result.slot;
+            stringValue_ = result.slot;
+    }
+
+    DataKind getKind() const {return channel_->dataKind.load(std::memory_order_acquire);}
+
+    std::unique_ptr<Dimensions> acceptLatestRequestedDimensions() {
+        return channel_->pendingDimensions.tryAcceptLatest();
     }
 };
 
